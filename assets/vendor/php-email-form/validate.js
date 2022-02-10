5b949e5911dfdd9db66885227d4216b23d840123
Matías <matiiias.-15@live.cl>
2022-02-10 12:15:14 -0300

    Avance envío correo contacto

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -59,6 +59,7 @@
       if( response.ok ) {
         return response.text()
       } else {
+        console.log(response);
         throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
       }
     })
